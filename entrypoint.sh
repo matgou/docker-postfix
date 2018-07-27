28ea72f9b5550e905bfc9071df7ade1d1c7377ef
Mathieu GOULIN <mathieu.goulin@gadz.org>
2018-07-27 13:11:11 +0000

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,7 +47,38 @@ sed -i "s:/var/log/mail:/var/log/postfix/mail:g" /etc/rsyslog.conf
 
 dovecot_ip=$( getent ahosts dovecot | awk '{ print $1 }' | head -1 )
 
+# Create spool directory structure
+mkdir -p /var/spool/postfix/{active,bounce,corrupt,defer,deferred,dev,etc,flush,incoming,lib,maildrop,pid,private,public,saved,usr}
+mkdir -p /var/spool/postfix/usr/lib/sasl2 /var/spool/postfix/usr/lib/zoneinfo
+chown postfix:root /var/spool/postfix/{active,bounce,corrupt,defer,deferred,flush,incoming,maildrop,private,public,saved}
+
+
+xdrwx------ 2 postfix root     4096 Jul 27 12:08 active
+drwx------ 2 postfix root     4096 Jul 27 12:08 bounce
+drwx------ 2 postfix root     4096 Jul 27 12:08 corrupt
+drwx------ 2 postfix root     4096 Jul 27 12:08 defer
+drwx------ 2 postfix root     4096 Jul 27 12:08 deferred
+drwxr-xr-x 2 root    root     4096 Feb 23 22:29 dev
+drwxr-xr-x 2 root    root     4096 Feb 23 22:29 etc
+drwx------ 2 postfix root     4096 Jul 27 12:08 flush
+drwx------ 2 postfix root     4096 Jul 27 12:08 incoming
+drwxr-xr-x 2 root    root     4096 Feb 23 22:29 lib
+drwx-wx--T 2 postfix postdrop 4096 Jul 27 12:08 maildrop
+drwxr-xr-x 2 root    root     4096 Jul 27 12:08 pid
+drwx------ 2 postfix root     4096 Jul 27 12:08 private
+drwx--s--- 2 postfix postdrop 4096 Jul 27 12:08 public
+drwx------ 2 postfix root     4096 Jul 27 12:08 saved
+drwxr-xr-x 3 root    root     4096 Jul 27 12:07 usr
+
+
+# Reploace dovecot with ip in main.cf
 sed -i "s:@@DOVECOT_IP@@:$dovecot_ip:" /etc/postfix/main.cf
 
+# If exist delete pid file
+rm /var/run/rsyslogd.pid
+
+# Start rsyslogd
 /usr/sbin/rsyslogd &
+
+# Start postfix on foreground
 /usr/lib/postfix/sbin/master -c /etc/postfix/ -d
